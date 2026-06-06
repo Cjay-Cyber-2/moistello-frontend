cbd0a27c24b7d716acfdb8fa2272646430cc5eb5
Nekwasar <lindawheek@gmail.com>
2026-06-06 12:54:24 +0200

    feat: add Yandex.Metrika tracking to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -129,6 +129,23 @@ export default function RootLayout({
              }),
            }}
          />
+         <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109691925', 'ym');
+ym(109691925, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+              `,
+            }}
+          />
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<div><img src="https://mc.yandex.ru/watch/109691925" style="position:absolute; left:-9999px;" alt="" /></div>`
+          }} />
         </head>
       <body
         className={`${spaceGrotesk.variable} ${inter.variable} ${jetbrainsMono.variable} font-body bg-[rgb(var(--background))] text-[rgb(var(--foreground))] antialiased`}
